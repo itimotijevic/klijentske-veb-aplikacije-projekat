d50ad53760a46f3d098de6feb649512742d2739d
itimotijevic <montidotabre@gmail.com>
2025-03-19 00:08:53 +0100

    update Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent},
+    { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: '' }
 ]
