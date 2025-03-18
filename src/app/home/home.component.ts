85a5d0f5deda860504ab0486b8a5308d66027b25
itimotijevic <montidotabre@gmail.com>
2025-03-18 20:23:12 +0100

    Implemented movie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { JsonPipe, NgIf } from '@angular/common';
+import { AxiosError } from 'axios';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [JsonPipe,NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  movies: any[] | null = null
+  error: string | null = null
 
+  constructor () {
+    MovieService.getMovies()
+    .then(rsp => this.movies = rsp.data)
+    .catch((e: AxiosError) => this.error = `${e.code} ${e.message}`)
+  }
 }
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,6 +1,35 @@
-export class MovieService {
-    static async getMovie()    {
+import axios from 'axios';
 
-        
+const client = axios.create({
+    baseURL: 'https://movie.pequla.com/api',
+    headers:     {
+        'Accept': 'application/json',
+        'X-CLient-Name': 'KVA/2025'
+    },    
+    validateStatus: (status: number) =>{
+        return status === 200 // samo ako je 200 vrati response
+                                // u ostalim slucajevima baci izuzetak
+    }
+})
+
+
+export class MovieService {
+    static async getMovies(page: number = 0,size: number = 10)    {
+        return client.request({
+            url: '/movie',
+            method: 'GET',
+            params: {
+                'page': page,
+                'size': size,
+                'sort': 'genre,asc', // nisam siguran da li treba po moveID i title ili po ovome????
+                'type': 'movie'
+
+            }
+        });
+              
+    }
+
+    static async getMovieById(id: number) {
+        return axios.get(`/movie/${id}`)  
     }
 }
